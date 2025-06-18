7db6bd9c851e76eb4e568c1b55adb4532107fcd9
teddythebear <nosaozemoyaefeanthony@gmail.com>
2025-06-18 10:52:13 +0100

    Update script.js

---
+++ the program/js/script.js
@@ -0,0 +1,4 @@
+const nam = document.getElementById("name");
+const add = document.getElementById("add");
+const reset = document.getElementById("reset");
+const delet = document.getElementById("delet");

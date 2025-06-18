bc4f6ad9170fbb07016c6b984a6f8ddeeb0c0466
teddythebear <nosaozemoyaefeanthony@gmail.com>
2025-06-18 11:18:37 +0100

    Update script.js
    made the program work with js

--- the program/js/script.js
+++ the program/js/script.js
@@ -2,3 +2,20 @@ const nam = document.getElementById("name");
 const add = document.getElementById("add");
 const reset = document.getElementById("reset");
 const delet = document.getElementById("delet");
+
+let ans = 0;
+
+add.onclick = function(){
+    ans++;
+    nam.textContent = ans;
+}
+
+reset.onclick = function(){
+    ans = 0 ;
+    nam.textContent = ans;
+}
+
+delet.onclick = function(){
+    ans--;
+    nam.textContent = ans;
+}
